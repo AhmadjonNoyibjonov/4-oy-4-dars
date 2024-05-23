acc989b7c925ae9ddfcbb118b70853187991b6ae
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-24 01:25:48 +0500

    project finishid

---
+++ js/index.js
@@ -0,0 +1,115 @@
+// 1-masala
+// let t = +prompt('poyezd jonash vaqti');
+// t >=0 && t <= 23;
+// let T = +prompt('kechkish vaqti kiritilsin');
+// T >= 1 && T <= 1000;
+// let jonash_vaqti;
+// jonash_vaqti = (t + (T % 24)) % 24;
+// console.log(jonash_vaqti);
+
+// 2-masala
+// let C = +prompt('gradus selsiyda harorat kiritilsin');
+// let K;
+// let F;
+// K = C + 273.15;
+// F = C * 1.80 + 32.00;
+// console.log(K);
+// console.log(F);
+
+// 3-masala
+// let Ali;
+// let Vali;
+// let K = +prompt('Ali Validan necha marta kattaligi kiritilsin');
+// let N = +prompt('Ali Validan qanchaga kattaligi kiritilsin');
+// Ali = Vali * K;
+// Vali = Ali - N;
+// console.log(Ali, Vali);
+
+// 4-masala
+// let n = +prompt("Bozordan olingan tarvuz kilosi");
+// let a = +prompt("Tarvuzdagi dastlabki suvning miqdori");
+// let b = +prompt("Tasvuzdagi suvning vaqt otkandagi miqdori");
+// let t_s_o;
+// let result;
+// t_s_o = (n - (n*a)/100);
+// result = t_s_o + (n * b)/100;
+// console.log(result);
+
+// 5-masala
+// let n = +prompt('2 dan katta natural son kiritilsin');
+// n >= 3; n >= 100;
+// let N;
+// N = n * (n - 2); 'Kopburchakning ichki burchaklari yigindisi';
+// console.log(N);
+
+// 6-masala
+// let hh1 = +prompt('soat kiritilsin');
+// let mm1 = +prompt('minut kiritilsin');
+// let t = +prompt('ozgarish vaqti kiritilsin');
+// let hh;
+// let mmm;
+// mm = (mm1 + t) % 60;
+// hh = hh1 + (Math.round((mm1 + t) / 60));
+// console.log('${hh} : ${mm}');
+
+// 7-masala
+// let B_Y = +prompt('Sardorning busining yoshi');
+// let S_Y = +prompt('Sardorning yoshi');
+// let k; 'Necha yildan song Sardorning yoshi buvisining yoshidan 2 marta kichik bolishi';
+// if ( (B_Y + k) / 2 == (S_Y + k) ) {
+//     console.log(k);
+// }
+
+// 8-masala
+
+// 9-masala
+// let n = +prompt('Qavariq kopburchakning tomonari soni');
+// let N; 'Dioganallari kesishish nuqtalari soni';
+// N = (n * (n - 1) * (n - 2) * (n - 3)) /24;
+// console.log(N);
+
+// 10-masala
+// let T = +prompt("Kinoning davomiyligi");
+// let A = +prompt("Video 1 sekuntining hajmi");
+// let B = +prompt('Video tortish tezligining sekuntiga miqdori');
+// let U_H;
+// let T_V;
+// let K_V;
+// let M_K_V;
+// U_H = T * A;
+// T_V = U_H / B;
+// K_V = A / B;
+// M_K_V = T_V - K_V;
+
+// console.log(M_K_V);
+
+
+
+// 12-masala
+// const usd = 11000.34;
+// const euro = 12354.03;
+// const bilet = $500;
+// const tolov = $250;
+// const m_va_k = '120 yevro';
+// let pul = +prompt('Alisherdagi pul somda kiritilsin');
+// if ((pul-(pul / usd == 750)) / euro >= 120) {
+//     console.log('Oq yol Alisher');
+// } else {
+//     console.log('ozgina sabr qil Alisher');
+// }
+
+
+// 13-masala
+// let a = +prompt('boyi kiritilsin');
+// let b = +prompt('eni kiritilsin');
+// let result;
+// let s;
+// let p;
+// p = 2*(a=b);
+// s = a*b;
+// if (s > p) {
+//     result = s;
+// } else {
+//     result = p;
+// }
+// console.log(result);
